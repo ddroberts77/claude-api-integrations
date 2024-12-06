d8b0013e762bdf33189bf8e7a342ad2639cc1026
ddroberts77 <34794318+ddroberts77@users.noreply.github.com>
2024-12-06 18:45:54 -0500

    Add TypeScript and ESLint configurations

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  coverageDirectory: 'coverage'
+};
